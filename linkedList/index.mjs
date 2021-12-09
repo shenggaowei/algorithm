c86589fb59ddb7574435650bf7bf1ed85176f478
gaogao <weishenggao@innobuddy.com>
2021-12-09 18:56:57 +0800

    feat: update linkedList

--- linkedList/index.mjs
+++ linkedList/index.mjs
@@ -8,23 +8,51 @@ export default class LinkedList {
     this.equalsFn = equalsFn;
   }
 
+  // 需要判断是插入第一个还是在最后进行插入
   push(element) {
     const node = new Node(element);
-    let current;
-    if (this.head === null) {
+    if (this.count === 0) {
       this.head = node;
     } else {
-      current = this.head;
-      while (current?.next != null) {
-        current = current.next;
+      let cur = this.head;
+      while (cur.next) {
+        cur = cur.next;
       }
-      current.next = node;
+      cur.next = node;
     }
     this.count++;
+  }
+
+  getElementAt(index) {
+    if (index >= 0 && index <= this.count) {
+      let node = this.head;
+      for (let i = 0; i < index; i++) {
+        node = node.next;
+      }
+      return node;
+    }
+    return undefined;
+  }
+
+  remove(index) {
+    if (index >= 0 && index < this.count) {
+      let cur = this.head;
+      if (index === 0) {
+        this.head = cur.next;
+      } else {
+        const pre = this.getElementAt(index - 1);
+        cur = pre.next;
+        pre.next = cur.next;
+      }
+      this.count--;
+    }
   }
 }
 
 const list = new LinkedList();
-list.push("哈哈");
-list.push("嘻嘻");
-console.log(list);
+list.push("嘻嘻1");
+list.push("哈哈2");
+list.push("嘻嘻3");
+console.log(JSON.stringify(list.head));
+list.remove(2);
+console.log(JSON.stringify(list.head));
